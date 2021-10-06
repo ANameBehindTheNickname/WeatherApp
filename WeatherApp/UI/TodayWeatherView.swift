9891fc5b6e349a25f995343d4c9f22ccc83231f5
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2021-10-06 11:44:37 +0300

    Add TodayWeatherView

---
+++ WeatherApp/UI/TodayWeatherView.swift
@@ -0,0 +1,28 @@
+//
+//
+//  Copyright © 2021 ANameBehindTheNickname. All rights reserved.
+//
+
+import UIKit
+
+final class TodayWeatherView: UIView {
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let nibName = String(describing: TodayWeatherView.self)
+        Bundle.main.loadNibNamed(nibName, owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+}
