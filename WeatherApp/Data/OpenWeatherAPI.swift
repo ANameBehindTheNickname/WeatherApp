c1f6b8be53d89bad7e346113571bbfa9426c86fa
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2021-10-07 15:23:21 +0300

    Remove duplication from OpenWeatherAPI

--- WeatherApp/Data/OpenWeatherAPI.swift
+++ WeatherApp/Data/OpenWeatherAPI.swift
@@ -30,43 +30,13 @@ final class OpenWeatherAPI: WeatherAPI {
     }
     
     func getCurrentWeather(for city: String, completion: @escaping (Weather) -> Void) {
-        let apiKey = "ENTER_YOUR_OPEN_WEATHER_API_KEY"
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=metric&appid=\(apiKey)")!
-        networkProvider.getData(from: url) { result in
-            switch result {
-            case .success(let weatherData):
-                guard let weatherResponse = try? JSONDecoder().decode(OpenWeatherResponse.self, from: weatherData) else {
-                    print("Couldn't parse WeatherResponse from weatherData")
-                    return
-                }
-                
-                let weather = weatherResponse.weather[0]
-                let main = weatherResponse.main
-                completion(Weather(location: weatherResponse.name, iconId: weather.icon, description: weather.description, currentTemperature: main.temp, minTemperature: main.temp_min, maxTemperature: main.temp_max))
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=metric")!
+        getCurrentWeather(from: url, completion: completion)
     }
     
     func getCurrentWeather(lat: String, lon: String, completion: @escaping (Weather) -> Void) {
-        let apiKey = "ENTER_YOUR_OPEN_WEATHER_API_KEY"
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&units=metric&appid=\(apiKey)")!
-        networkProvider.getData(from: url) { result in
-            switch result {
-            case .success(let weatherData):
-                guard let weatherResponse = try? JSONDecoder().decode(OpenWeatherResponse.self, from: weatherData) else {
-                    print("Couldn't parse WeatherResponse from weatherData")
-                    return
-                }
-                
-                let weather = weatherResponse.weather[0]
-                let main = weatherResponse.main
-                completion(Weather(location: weatherResponse.name, iconId: weather.icon, description: weather.description, currentTemperature: main.temp, minTemperature: main.temp_min, maxTemperature: main.temp_max))
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&units=metric")!
+        getCurrentWeather(from: url, completion: completion)
     }
     
     func getWeatherIconData(iconId: String, completion: @escaping (Data) -> Void) {
@@ -77,6 +47,26 @@ final class OpenWeatherAPI: WeatherAPI {
                 completion(iconData)
             case .failure(let error):
                 print("Couldn't get image data - \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func getCurrentWeather(from url: URL, completion: @escaping (Weather) -> Void) {
+        let apiKey = "ENTER_YOUR_OPEN_WEATHER_API_KEY"
+        let newURL = URL(string: url.absoluteString + "&appid=\(apiKey)")!
+        networkProvider.getData(from: newURL) { result in
+            switch result {
+            case .success(let weatherData):
+                guard let weatherResponse = try? JSONDecoder().decode(OpenWeatherResponse.self, from: weatherData) else {
+                    print("Couldn't parse WeatherResponse from weatherData")
+                    return
+                }
+                
+                let weather = weatherResponse.weather[0]
+                let main = weatherResponse.main
+                completion(Weather(location: weatherResponse.name, iconId: weather.icon, description: weather.description, currentTemperature: main.temp, minTemperature: main.temp_min, maxTemperature: main.temp_max))
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
     }
